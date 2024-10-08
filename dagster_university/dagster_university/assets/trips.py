2854a8597ff24b17614aa20b27994487f995beb8
conanmercer <conan.mercer@sage.com>
2024-10-08 10:22:40 +0200

    add, my first asset

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,2 +1,17 @@
 import requests
 from . import constants
+from dagster import asset
+
+
+@asset
+def taxi_trips_file() -> None:
+    """The raw parquet files for the taxi trips dataset. Data is sourced from the NYC Taxi and Limousine Commission."""
+    month_to_fetch = "2023-02"
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+
+    with open(
+        constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb"
+    ) as output_file:
+        output_file.write(raw_trips.content)
