48e458246f0e3c82f3e730cba2fc9c4504c90568
conanmercer <conan.mercer1@gmail.com>
2024-10-10 15:50:35 +0200

    add, new asset

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,4 +1,6 @@
 import requests
+import duckdb
+import os
 from . import constants
 from dagster import asset
 
@@ -28,3 +30,31 @@ def taxi_zones_file() -> None:
 
     with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
         output_file.write(raw_taxi_zones.content)
+
+
+@asset(
+    deps=["taxi_trips_file"]
+)
+def taxi_trips() -> None:
+    """
+      The raw taxi trips dataset, loaded into a DuckDB database
+    """
+    sql_query = """
+        create or replace table trips as (
+          select
+            VendorID as vendor_id,
+            PULocationID as pickup_zone_id,
+            DOLocationID as dropoff_zone_id,
+            RatecodeID as rate_code_id,
+            payment_type as payment_type,
+            tpep_dropoff_datetime as dropoff_datetime,
+            tpep_pickup_datetime as pickup_datetime,
+            trip_distance as trip_distance,
+            passenger_count as passenger_count,
+            total_amount as total_amount
+          from 'data/raw/taxi_trips_2023-02.parquet'
+        );
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+    conn.execute(sql_query)
